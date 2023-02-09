1f7931c242a0db5290e1da00914ca8fc0b3c34e9
Horacio Tellez <hote@odoo.com>
2023-02-09 10:07:11 +0100

    adding generics to generators

--- src/io_utils.rs
+++ src/io_utils.rs
@@ -1,20 +1,18 @@
-use hound::{WavReader, WavSpec, WavWriter};
+use hound::{WavReader, WavSamples, WavSpec, WavWriter};
 
 use std::f32::consts::PI;
 use std::fs::File;
 use std::i16;
 use std::io::BufReader;
 
-pub type SampleGeneratorI16 = fn(usize) -> i16;
-
 pub fn read(filename: &str) -> WavReader<BufReader<File>> {
     WavReader::open(filename).unwrap()
 }
 
-pub fn write(
+pub fn write<SG: Fn(usize) -> i16>(
     filename: &str,
     spec: WavSpec,
-    sg: SampleGeneratorI16,
+    sg: SG,
     duration: usize,
 ) -> Result<(), hound::Error> {
     let mut writer = WavWriter::create(filename, spec).unwrap();
@@ -44,4 +42,22 @@ pub fn simple_sg(t: usize) -> i16 {
     }
 
     return (sample * amplitude) as i16;
+}
+
+pub fn create_sample_generator_from_reader(
+    reader: &mut WavReader<BufReader<File>>,
+) -> impl FnMut(usize) -> i16 + '_ {
+    let mut samples: WavSamples<BufReader<File>, i16> = reader.samples();
+
+    let samples_length = samples.len();
+
+    let f = move |t: usize| {
+        if t < samples_length {
+            samples.next().unwrap().unwrap()
+        } else {
+            0 as i16
+        }
+    };
+
+    f
 }
